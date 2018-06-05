2822d12be1ffc4384b809bfd61f385eb5ff369f1
Jake Harris <harri676@purdue.edu>
2018-06-05 13:57:01 -0400

    Give classes to each part of the list element and gave them different styles. In addition made functions to create each element

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-console.log("testing")
 const button = document.querySelector('#change')
 const title = document.querySelector('#title')
 const description = document.querySelector('#desc')
@@ -7,18 +6,45 @@ const submit = document.querySelector('#sub')
 const form = document.querySelector('#form')
 const level = document.querySelector('#level')
 
-const changeHeading = function() {
-    title.textContent = 'Hey look my button works!'
-    description.textContent = 'Wow it must be magic or something!'
+const createSpan = function(type) {
+    if(type === 'spell'){
+        const spellNameNode = document.createElement('SPAN')
+        const spellAttr = document.createAttribute('class')
+        const spellName = document.createTextNode(`${textField.value}`)
+        spellAttr.value = 'spell'
+        spellNameNode.setAttributeNode(spellAttr)
+        spellNameNode.appendChild(spellName)
+        return spellNameNode
+    }else if(type === 'level'){
+        const spellLevelNode = document.createElement('SPAN')
+        const levelAttr = document.createAttribute('class')
+        const spellLevel = document.createTextNode(` - ${level.value}`)
+        levelAttr.value = 'level'
+        spellLevelNode.setAttributeNode(levelAttr)
+        spellLevelNode.appendChild(spellLevel)
+        return spellLevelNode
+    }else{
+        const spellNameNode = document.createElement('SPAN')
+        const spellAttr = document.createAttribute('class')
+        const spellName = document.createTextNode(`${textField.value}`)
+        spellAttr.value = 'spell'
+        spellNameNode.setAttributeNode(spellAttr)
+        spellNameNode.appendChild(spellName)
+        return spellNameNode
+    }
+}
+
+const createLi = function() {
+    const node = document.createElement('LI')
+    const spellNameNode = createSpan('spell')
+    const spellLevelNode = createSpan('level')
+    node.appendChild(spellNameNode)
+    node.appendChild(spellLevelNode)
+    document.querySelector('#spells').appendChild(node)
 }
 
 form.onsubmit = function(event) {
-    const node = document.createElement('LI')
-    const spellItem = document.createTextNode(`${textField.value} - ${level.value}`)
-    node.appendChild(spellItem)
-    document.querySelector('#spells').appendChild(node) 
+    createLi()
     textField.value = ''
     return false
 }
-
-button.addEventListener('click', changeHeading)
